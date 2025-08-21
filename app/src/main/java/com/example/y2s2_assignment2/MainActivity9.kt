5712e3cda2869ddeae68ab71d5fadd948b9b888c
Theshan <theshangeethanjana@gmail.com>
2025-08-22 00:36:30 +0530

    Home page halfly completed

--- app/src/main/java/com/example/y2s2_assignment2/MainActivity9.kt
+++ app/src/main/java/com/example/y2s2_assignment2/MainActivity9.kt
@@ -3,25 +3,26 @@ package com.example.y2s2_assignment2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity6 : AppCompatActivity() {
+class MainActivity9 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main6)
+        setContentView(R.layout.activity_main9)
 
+//        val signinbutton = findViewById<ImageView>(R.id.imageView15)
+//
+//        signinbutton.setOnClickListener(){
+//
+//            val intent = Intent(this,MainActivity6::class.java)
+//            startActivity(intent)
+//        }
 
-        val signinbutton = findViewById<Button>(R.id.button2)
-
-        signinbutton.setOnClickListener(){
-
-            val intent = Intent(this,MainActivity9::class.java)
-            startActivity(intent)
-        }
 
     }
 }
