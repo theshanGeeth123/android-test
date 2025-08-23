bf2fb2f264d822e36d1715dc5bc70adf363bbfb9
Theshan <theshangeethanjana@gmail.com>
2025-08-23 15:11:03 +0530

    payment successful page has been completed

--- app/src/main/java/com/example/y2s2_assignment2/MainActivity14.kt
+++ app/src/main/java/com/example/y2s2_assignment2/MainActivity14.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class NabBar : AppCompatActivity() {
+class MainActivity14 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_nab_bar)
+        setContentView(R.layout.activity_main14)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
