66292c32ed1e91e08ade5a996f46f95702fafae4
Theshan <theshangeethanjana@gmail.com>
2025-08-24 15:29:51 +0530

    Up to product2 completed

---
+++ app/src/main/java/com/example/y2s2_assignment2/MainActivity16.kt
@@ -0,0 +1,18 @@
+package com.example.y2s2_assignment2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity16 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main16)
+
+
+
+    }
+}
